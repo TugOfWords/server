ba7741c06ebee1f8801ee14a7b86d7fbb9a58f77
Charlie Crouse <crouse0@users.noreply.github.com>
2018-02-07 18:41:44 -0500

    Add server side user creation (#4)

---
+++ modules/user.js
@@ -0,0 +1,18 @@
+const firebase = require('../fire');
+
+/**
+ * Creates a new entry at the /users/:id firebase endpoint with the given username
+ * @param {String} uid
+ *   the unique id that identifies the user in our database
+ * @param {String} username
+ *   the in game name the user has chosen
+ */
+const createUser = (uid, username) => {
+  firebase.ref(`users/${uid}`).set({
+    username,
+  });
+};
+
+module.exports = {
+  createUser,
+};
