ad81d9dcf07807a72c1e48018a2b3e7f767f3011
Anoop Jain <anoopj08@gmail.com>
2018-02-07 20:54:02 -0500

    added random gen code, need to add code to make sockets work

--- app.js
+++ app.js
@@ -7,30 +7,32 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+var randomWords = require('random-words')
+
 /**
 * Connection handler for the websocket
 * @param {Object} socket
 *   the communication channel between the client and the server
 */
 const onConnection = (socket) => {
- // client disconnect handler
- socket.on('createUser', () => {
+// client disconnect handler
+  socket.on('createUser', () => {
 
- });
+  });
 
-socket.on('joinRoom', () => {
-     socket.join(uid);
-});
+  // socket.on('joinRoom', () => {
+  //   socket.join(uid);
+  // });
 
-socket.on('senWord', () => {
-
-});
+  socket.on('sendWord', () => {
+    //TODO: uncomment and send getWord()
+  });
 
   socket.on('disconnect', () => console.log('Client has disconnected'));
 };
 
 // function getWord(){
-
+//   return randomWords();
 // }
 
 // initialize socket handler
