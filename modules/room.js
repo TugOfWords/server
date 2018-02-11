845dacb8411ed3a26ac42d6abc7156acb6c53c4b
Jenna Ellis <ellis93@purdue.edu>
2018-02-11 18:22:21 -0500

    Add tests for user and room modules (#8)

--- modules/room.js
+++ modules/room.js
@@ -2,10 +2,13 @@ const firebase = require('../fire');
 const express = require('express');
 
 const roomRouter = express.Router();
+
 /**
  * Creates a new entry at the /rooms/:id firebase endpoint with the given roomname
  * @param {String} rid
  *   the unique id that identifies the room in our database
+ * @param {String} owner
+ *   the uid of the user that created the room
  */
 const createRoom = (rid, owner) => {
   firebase.ref(`rooms/${rid}`).set({
