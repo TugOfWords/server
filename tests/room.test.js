4f7a0c95b983b78e2e865e2ad01b2ddfba75889f
pkoushik <pkoushik11@gmail.com>
2018-02-11 20:39:06 -0500

    add room test

--- tests/room.test.js
+++ tests/room.test.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const firebase = require('../fire');
 const { createRoom } = require('../modules/room');
 
@@ -5,8 +6,20 @@ describe('Tests for room module', () => {
   it('should create a room in the firebase database', () => {
     const dummyId = 'create-room-test-id';
     createRoom(dummyId, 'some-id');
-    // TODO: assert that the room has actually been added to firebase
+    const currRoom = firebase.ref(`rooms/${dummyId}`);
+    if (!currRoom) {
+      assert(null);
+    }
+    firebase.ref(`rooms/${dummyId}`).remove();
+  });
 
+  it('should remove a room in the firebase database', () => {
+    const dummyId = 'remove-room-test-id';
+
+    // Add a dummy room to remove
+    firebase.ref(`rooms/${dummyId}`).set({
+      username: 'remove-room-test',
+    });
     firebase.ref(`rooms/${dummyId}`).remove();
   });
 });
