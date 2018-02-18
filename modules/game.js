4a39ad8ecca86c55bc09e4de2675f047c6f86d08
Anoop Jain <anoopj08@gmail.com>
2018-02-17 20:50:28 -0500

    init

--- modules/game.js
+++ modules/game.js
@@ -1,4 +1,5 @@
 const randomWords = require('random-words');
+const firebase = require('../fire');
 
 /**
  * Generates a random word
@@ -7,6 +8,20 @@ const randomWords = require('random-words');
  */
 const getWord = () => randomWords();
 
+/**
+ * Adds a point for the user at the users/{uid} endpoint
+ * @param {String} uid
+ *   the uid of the user that deserves a point
+ */
+const addPoint = (uid) => {
+  let currUserPoints = firebase.ref(`users/${uid}/points`);
+  const currUser = firebase.ref(`users/${uid}`);
+  currUserPoints += 1;
+  currUser.update({ points: currUserPoints });
+};
+
+
 module.exports = {
   getWord,
+  addPoint,
 };
