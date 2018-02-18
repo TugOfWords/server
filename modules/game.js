3a19af6ba58e94e7be00ec8f338b0d3b929b7f01
Prashanth Koushik <pkoushik11@gmail.com>
2018-02-18 16:21:59 -0500

    Pk game test (#11)
    * test random word generators
    
    * take out logs
    
    * Aj add point, remove point, users created with a points field (#10)
    
    * init
    
    * add point and subtract point features done in game.js
    
    * i fukt up
    
    * added points field to users
    
    * commit to switch
    
    * point test done
    
    * remove users from firebase for points tests
    
    * change random word generator library

--- modules/game.js
+++ modules/game.js
@@ -1,12 +1,36 @@
-const randomWords = require('random-words');
+const randomWords = require('rword');
+const firebase = require('../fire');
 
 /**
  * Generates a random word
  * @returns {String}
  *   the generated word
  */
-const getWord = () => randomWords();
+const getWord = () => randomWords.generate();
+
+/**
+ * Adds a point for the user at the users/{uid} endpoint
+ * @param {String} uid
+ *   the uid of the user that deserves a point
+ */
+const addPoint = (uid) => {
+  const ref = firebase.ref(`users/${uid}/points`);
+  ref.transaction(currPoints => (currPoints || 0) + 1);
+};
+
+/**
+ * Removes a point for the user at the users/{uid} endpoint
+ * @param {String} uid
+ *   the uid of the user that deserves a point
+ */
+const removePoint = (uid) => {
+  const ref = firebase.ref(`users/${uid}/points`);
+  ref.transaction(currPoints => (currPoints || 0) - 1);
+};
+
 
 module.exports = {
   getWord,
+  addPoint,
+  removePoint,
 };
