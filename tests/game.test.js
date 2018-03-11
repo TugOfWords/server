4eb2a62c5d00cc13e6e38beb90057e4d94d644d5
Anoop Jain <anoopj08@gmail.com>
2018-03-11 18:03:44 -0400

    k fixed everything

--- tests/game.test.js
+++ tests/game.test.js
@@ -1,9 +1,8 @@
 const assert = require('assert');
-// const firebase = require('../fire');
-// const { getWord, addPoint, removePoint } = require('../modules/game');
-const { getWord } = require('../modules/game');
-// const { createUser } = require('../modules/user');
-// const { createLobby, joinLobby } = require('../modules/lobby');
+const firebase = require('../fire');
+const { getWord, addPoint, removePoint } = require('../modules/game');
+const { createUser } = require('../modules/user');
+const { createLobby, joinLobby } = require('../modules/lobby');
 
 
 describe('Tests for game module', () => {
@@ -35,35 +34,37 @@ describe('Tests for game module', () => {
     }
   });
 
-  // it('should add points to a certain user', async () => {
-  //   const uid = 'add-point-test-uid';
-  //   const lid = 'add-point-test-lid';
-  //   await createUser(uid, 'add-points-test-username');
-  //   await createLobby(lid, uid);
-  //   await joinLobby(lid, uid);
-  //   let snapshot = await firebase.ref(`/lobbys/${lid}/users/${uid}`).once('value');
-  //   const bp = snapshot.val();
-  //   await addPoint(lid, uid);
-  //   snapshot = await firebase.ref(`/lobbys/${lid}/users/${uid}`).once('value');
-  //   const ap = snapshot.val().points;
-  //   // assert((ap - bp) === 1);
-  //   firebase.ref(`/lobbys/${lid}`).remove();
-  //   firebase.ref(`/users/${uid}`).remove();
-  // });
+  it('should add points to a certain user', async () => {
+    const uid = 'add-point-test-uid';
+    const lid = 'add-point-test-lid';
+    await createUser(uid, 'add-points-test-username');
+    await createLobby(lid, uid);
+    await joinLobby(lid, uid);
+    let snapshot = await firebase.ref(`/lobbys/${lid}/users/${uid}`).once('value');
+    const bp = snapshot.val().points;
+    await addPoint(lid, uid);
+    snapshot = await firebase.ref(`/lobbys/${lid}/users/${uid}`).once('value');
+    const ap = snapshot.val().points;
+    assert((ap - bp) === 1);
+    firebase.ref(`/lobbys/${lid}`).remove();
+    firebase.ref(`/users/${uid}`).remove();
+  });
 
-  // it('should remove points to a certain user', async () => {
-  //   const uid = 'remove-point-test-uid';
-  //   const lid = 'remove-point-test-lid';
-  //   createUser(uid, 'remove-points-test-username');
-  //   createLobby(lid, uid);
-  //   joinLobby(lid, uid);
-  //   let snapshot = await firebase.ref(`/lobbys/${lid}/users/${uid}`).once('value');
-  //   const bp = snapshot.val().points;
-  //   await removePoint(lid, uid);
-  //   snapshot = await firebase.ref(`/lobbys/${lid}/users/${uid}`).once('value');
-  //   const ap = snapshot.val().points;
-  //   // assert((bp - ap) === 1);
-  //   firebase.ref(`/lobbys/${lid}`).remove();
-  //   firebase.ref(`/users/${uid}`).remove();
-  // });
+  it('should remove points to a certain user', async () => {
+    const uid = 'remove-point-test-uid';
+    const lid = 'remove-point-test-lid';
+    await createUser(uid, 'remove-points-test-username');
+    await createLobby(lid, uid);
+    await joinLobby(lid, uid);
+    await addPoint(lid, uid);
+    await addPoint(lid, uid);
+    let snapshot = await firebase.ref(`/lobbys/${lid}/users/${uid}`).once('value');
+    const bp = snapshot.val().points;
+    await removePoint(lid, uid);
+    snapshot = await firebase.ref(`/lobbys/${lid}/users/${uid}`).once('value');
+    const ap = snapshot.val().points;
+    assert((ap - bp) === -1);
+    firebase.ref(`/lobbys/${lid}`).remove();
+    firebase.ref(`/users/${uid}`).remove();
+  });
 });
